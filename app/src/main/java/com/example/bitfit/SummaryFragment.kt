1dd149b090622c26e29d099c3795c71b213dea05
MrJigglys <djtheman007@gmail.com>
2025-03-11 21:48:30 -0400

    Completed lab. Added demo.gif and README.md

--- app/src/main/java/com/example/bitfit/SummaryFragment.kt
+++ app/src/main/java/com/example/bitfit/SummaryFragment.kt
@@ -5,10 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class SummaryFragment : Fragment() {
-
+    private var sleeps = mutableListOf<Sleep>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +23,25 @@ class SummaryFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_summary, container, false)
         val layoutManager = LinearLayoutManager(context)
 
-        val totalHoursText: View = view.findViewById(R.id.totalHoursText)
-        val averageHoursText: View = view.findViewById(R.id.averageHoursText)
-        // Get info from database and insert into textViews
+        val totalHoursText: TextView = view.findViewById(R.id.totalHoursText)
+        val averageHoursText: TextView = view.findViewById(R.id.averageHoursText)
+
+        sleeps = (activity as MainActivity).getDB()
+
+        // Get hours and minutes
+        var hSum = 0.0
+        var mSum = 0.0
+        for (item in sleeps) {
+            if (!item.hours.isNullOrBlank())
+                hSum+=item.hours.toInt()
+            if (!item.minutes.isNullOrBlank())
+                mSum+=item.minutes.toInt()
+        }
+        // Convert minutes to hours
+        hSum+=(mSum/60)
+        var hAvg = hSum/sleeps.size
+        totalHoursText.text = "Total Hours: " + hSum.toString()
+        averageHoursText.text = "Average Hours: " + hAvg.toString()
 
         return view
     }
